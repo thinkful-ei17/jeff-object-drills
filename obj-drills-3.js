b198130b62cbb4b9ad27c8a3d6e3fa6c8dd89c21
Jeff Connelly <jeff.connelly6@gmail.com>
2017-12-06 13:13:36 -0700

    Added Drill #5

--- obj-drills-3.js
+++ obj-drills-3.js
@@ -40,35 +40,73 @@ console.log(hobbiton.meals[3]);
 
 //Arrays of objects
 
+// const starWars = {
+//   name: 'Luke',
+//   jobTitle: 'Jedi'
+// };
+
+// const starWars2 = {
+//   name: 'Han Solo',
+//   jobTitle: 'Legendary Smuggler'
+// };
+
+// const starWars3 = {
+//   name: 'Leia',
+//   jobTitle: 'Resistance Leader'
+// };
+
+// const starWars4 = {
+//   name: 'Chewie',
+//   jobTitle: 'Best 2nd Mate'
+// };
+
+// function storeTheChars() {
+//   let mainCast = [];
+//   mainCast.push(starWars, starWars2, starWars3, starWars4);
+//   console.log(mainCast.map(char => `${char.name}: ${char.jobTitle}`));
+// }
+
+// storeTheChars();
+
+//Properties that aren't there
+
+
 const starWars = {
   name: 'Luke',
-  jobTitle: 'Jedi'
+  jobTitle: 'Jedi',
 };
 
 const starWars2 = {
   name: 'Han Solo',
-  jobTitle: 'Legendary Smuggler'
+  jobTitle: 'Legendary Smuggler',
+  boss: 'Luke'
 };
 
 const starWars3 = {
   name: 'Leia',
-  jobTitle: 'Resistance Leader'
+  jobTitle: 'Resistance Leader',
+  boss: 'Luke',
 };
 
 const starWars4 = {
   name: 'Chewie',
-  jobTitle: 'Best 2nd Mate'
+  jobTitle: 'Best 2nd Mate',
+  boss: 'Han Solo'
 };
 
 function storeTheChars() {
   let mainCast = [];
   mainCast.push(starWars, starWars2, starWars3, starWars4);
-  console.log(mainCast.map(char => `${char.name}: ${char.jobTitle}`));
+  // let key = 'boss';
+  // if (mainCast.filter(check => key in check)) {
+  console.log(mainCast.map(char => {
+    if (char.boss === undefined) {
+      return `${char.jobTitle} ${char.name} doesn't report to anybody`;
+    }
+    else {
+      return `${char.jobTitle} ${char.name} reports to ${char.boss}`;
+    }
+  }));
 }
 
 storeTheChars();
-
-//Properties that aren't there
-
-
-
